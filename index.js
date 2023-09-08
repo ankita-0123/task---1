fe6b03df60f10da7257f498ab3937af0609c21a2
ankita-0123 <pragyanparamita1449@gmail.com>
2023-09-08 17:16:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static files from the 'build' directory (for production)
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle routes for your React app (for production)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
